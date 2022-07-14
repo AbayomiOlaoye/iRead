92b3a27e81c0ec950749a625022f7467a3e5a596
Olaoye Oluwatoyin <mindprepopps@gmail.com>
2022-07-14 17:30:24 +0100

    Fix undefined data type of newStack

--- main.js
+++ main.js
@@ -42,3 +42,48 @@ window.addEventListener('click', (e) => {
     });
   }
 });
+
+// // Declaring and initiating an empty array for book collection as user fills the form.
+const iReadShelf = [];
+
+// Constructor function for book objects
+function Book(title, author, pages, status, theme) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+  this.theme = theme;
+  this.info = function commendReader() {
+    return `A book written piece by ${this.author}.`;
+  };
+}
+
+// Collection of inputs to be used as objects
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const bookPages = document.querySelector('#pages');
+const bookStatus = document.querySelector('#status');
+const bookTheme = document.querySelector('#theme');
+const shelfBook = document.querySelector('.shelf-book');
+let newStack;
+function addBooks() {
+  newStack = new Book(bookTitle.value, bookAuthor.value, bookPages.value,
+    bookStatus.value, bookTheme.value);
+  return iReadShelf.push(newStack);
+}
+
+shelfBook.addEventListener('click', (e) => {
+  e.preventDefault();
+  addBooks();
+  form.style.display = 'none';
+});
+
+// Designing the book cards
+// function designBookShelf(book) {
+//   return `<div class="book-card">
+//     <h2>${iReadShelf[0].title}</h2>
+//   </div>`;
+// }
+
+// Writing expressions to put books on shelf
+// document.querySelector('.container').innerHTML = ``
